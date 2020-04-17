85abedb18998caaa5fad50cac7d2dcd9a2a0375d
arun <arun.shreram@gmail.com>
2020-04-17 07:30:33 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+ 
+export default class Footer extends Component {
+  render() {
+    return (
+      <React.Fragment>
+
+<footer class="page-footer font-small bg-primary ">
+
+<div class="footer-copyright text-center ">© 2020 Copyright:
+  </div>
+  
+</footer>
+</React.Fragment>
+    )
+  }
+}
+
